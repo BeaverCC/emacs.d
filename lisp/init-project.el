7f4c03b9422bda1fda48ed3f434b12b565885c0b
beaver <beaveratwork@gmail.com>
2015-10-23 19:23:36 +0800

    move line up down

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -23,4 +23,30 @@
 (global-hl-line-mode 1)
 (global-linum-mode t)
 
+(defun move-line (n)
+  "Move the current line up or down by N lines."
+  (interactive "p")
+  (setq col (current-column))
+  (beginning-of-line) (setq start (point))
+  (end-of-line) (forward-char) (setq end (point))
+  (let ((line-text (delete-and-extract-region start end)))
+    (forward-line n)
+    (insert line-text)
+    ;; restore point to original column in moved line
+    (forward-line -1)
+    (forward-char col)))
+
+(defun move-line-up (n)
+  "Move the current line up by N lines."
+  (interactive "p")
+  (move-line (if (null n) -1 (- n))))
+
+(defun move-line-down (n)
+  "Move the current line down by N lines."
+  (interactive "p")
+  (move-line (if (null n) 1 n)))
+
+(global-set-key (kbd "M-P") 'move-line-up)
+(global-set-key (kbd "M-N") 'move-line-down)
+
 (provide 'init-project)
