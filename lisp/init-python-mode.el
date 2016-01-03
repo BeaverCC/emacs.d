f2bb0f40b301e1df4f6885c54e042e3a6fd79a31
beaver <beaveratwork@gmail.com>
2016-01-03 21:43:52 +0800

    eply

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -1,9 +1,16 @@
-(setq auto-mode-alist
-      (append '(("SConstruct\\'" . python-mode)
-		("SConscript\\'" . python-mode))
-              auto-mode-alist))
+;; (setq auto-mode-alist
+;;       (append '(("SConstruct\\'" . python-mode)
+;; 		("SConscript\\'" . python-mode))
+;;               auto-mode-alist))
 
-(require-package 'pip-requirements)
+;; (require-package 'pip-requirements)
 
+;; (defvar myPackages
+;;   '(better-defaults
+;;     elpy ;; add the elpy package
+;;     material-theme))
+(require-package 'elpy)
+
+(elpy-enable)
 
 (provide 'init-python-mode)
